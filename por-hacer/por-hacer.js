b9c626bbc5ba121f72a9b7e2cbbb8c01df88b7b1
JUAN DIEGO <JOLIVEROS@IEO.COM.CO>
2018-11-02 20:24:42 -0500

    Se desarrolla la opcion de escrivir varias tareas en el mismo archivo json

--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 let listadoPorHcer = [];
 
 const guardarDB = () => {
+    cargarDB();
     let data = JSON.stringify(listadoPorHcer);
-
     //escribimos la tabla del 2 en un archivo externo
     fs.writeFile(`db/data.json`, data, (err) => {
         if (err) throw new Error('No se pudo grabar', err);
@@ -12,7 +12,17 @@ const guardarDB = () => {
 
 };
 
+const cargarDB=()=>{
+    try{
+        listadoPorHcer=require('../db/data.json');
+    }
+    catch (e) {
+        listadoPorHcer=[];
+    }
+};
+
 const crear = (descripcion) => {
+    cargarDB();
     let porHacer = {
         descripcion,
         completado: false
